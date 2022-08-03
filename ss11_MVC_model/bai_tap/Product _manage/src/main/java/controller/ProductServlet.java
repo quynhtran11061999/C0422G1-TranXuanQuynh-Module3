9a8dccbc7455d4ce43850dfe27f98b35bdda08d6
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-08-03 09:15:17 +0700

    update

---
+++ ss11_MVC_model/bai_tap/Product _manage/src/main/java/controller/ProductServlet.java
@@ -0,0 +1,209 @@
+package controller;
+
+import model.Product;
+import service.IProductService;
+import service.impl.ProductService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProductServlet", urlPatterns = {"", "/products"})
+public class ProductServlet extends HttpServlet {
+    private IProductService productService = new ProductService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "add":
+                showAddForm(request, response);
+                break;
+            case "update":
+                showUpdateForm(request, response);
+                break;
+            case "delete":
+                deleteProduct(request, response);
+                break;
+            case "viewProduct":
+                viewProduct(request, response);
+                break;
+            case "searchByName":
+                break;
+            default:
+                listProduct(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "add":
+                addProduct(request, response);
+                break;
+            case "update":
+                updateProduct(request, response);
+                break;
+            case "delete":
+                deleteProduct(request, response);
+                break;
+            case "viewProduct":
+                break;
+            case "searchByName":
+                searchProduct(request, response);
+                break;
+            default:
+                listProduct(request, response);
+        }
+    }
+
+    private void viewProduct(HttpServletRequest request, HttpServletResponse response) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product product = this.productService.searchById(id);
+        RequestDispatcher requestDispatcher;
+            request.setAttribute("product", product);
+            requestDispatcher = request.getRequestDispatcher("view/product/view.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showUpdateForm(HttpServletRequest request, HttpServletResponse response) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product product = this.productService.searchById(id);
+        RequestDispatcher requestDispatcher;
+
+            request.setAttribute("product", product);
+            requestDispatcher = request.getRequestDispatcher("view/product/update.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAddForm(HttpServletRequest request, HttpServletResponse response) {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view/product/add.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listProduct(HttpServletRequest request, HttpServletResponse response) {
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view/product/display.jsp");
+        List<Product> productList = productService.display();
+        request.setAttribute("products", productList);
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void searchProduct(HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("name");
+        List<Product> products = productService.searchByName(name);
+        request.setAttribute("products", products);
+        request.setAttribute("flag", 1);
+        try {
+            request.getRequestDispatcher("view/product/display.jsp").forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteProduct(HttpServletRequest request, HttpServletResponse response) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Product product = this.productService.searchById(id);
+        RequestDispatcher requestDispatcher;
+
+            this.productService.delete(id);
+            request.setAttribute("message", "Xóa thành công !!!");
+            requestDispatcher = request.getRequestDispatcher("view/product/display.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateProduct(HttpServletRequest request, HttpServletResponse response) {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        Double price = Double.parseDouble(request.getParameter("price"));
+        String describe = request.getParameter("describe");
+        String producer = request.getParameter("producer");
+        Product product = this.productService.searchById(id);
+        RequestDispatcher requestDispatcher;
+
+            product.setId(id);
+            product.setName(name);
+            product.setPrice(price);
+            product.setDescribe(describe);
+            product.setProducer(producer);
+            this.productService.update(id, product);
+            request.setAttribute("product", product);
+            request.setAttribute("message", "Sửa thông tin sản phầm thành công");
+            requestDispatcher = request.getRequestDispatcher("view/product/update.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addProduct(HttpServletRequest request, HttpServletResponse response) {
+        int id = (int) (Math.random() * 10000);
+        String name = request.getParameter("name");
+        Double price = Double.parseDouble(request.getParameter("price"));
+        String describe = request.getParameter("describe");
+        String producer = request.getParameter("producer");
+
+        Product product = new Product(id, name, price, describe, producer);
+        this.productService.add(product);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view/product/add.jsp");
+        request.setAttribute("message", "Thêm mới sản phầm thành công");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ss11_MVC_model/bai_tap/Product _manage/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: quynh
+  Date: 29/07/2022
+  Time: 3:45 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>$Title$</title>
+</head>
+<body>
+<c:redirect url="/products"></c:redirect>
+</body>
+</html>
