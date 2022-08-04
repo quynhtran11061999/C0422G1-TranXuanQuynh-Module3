8867e0451b5712a0ea435021cca95188145cb73b
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-08-04 17:29:55 +0700

    update

---
+++ CaseStudy/case_study/src/main/java/controller/FuramaServlet.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "FuramaServlet", value = "/furama")
+public class FuramaServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view/home.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ CaseStudy/case_study/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: quynh
+  Date: 04/08/2022
+  Time: 9:06 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<html>
+  <head>
+    <title>Furama</title>
+  </head>
+  <body>
+  <c:redirect url="/furama"></c:redirect>
+  </body>
+</html>
